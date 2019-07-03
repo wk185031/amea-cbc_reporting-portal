a6ffdbf8f5b85cbf457ae4b999382a9c14e0b986
karmun <karmun.chai@mandrill.com.my>
2019-07-03 17:11:23 +0800

    #22, #23, #24

---
+++ src/main/java/my/com/mandrill/base/reporting/billingAllocationReportsInterBank/AcquirerBalanceInquiryTransactionFees.java
@@ -0,0 +1,7 @@
+package my.com.mandrill.base.reporting.billingAllocationReportsInterBank;
+
+import my.com.mandrill.base.reporting.reportProcessor.CsvReportProcessor;
+
+public class AcquirerBalanceInquiryTransactionFees extends CsvReportProcessor {
+
+}
---
+++ src/main/java/my/com/mandrill/base/reporting/billingAllocationReportsInterEntity/InterEntityIssuerBalanceInquiryTransactionFees.java
@@ -0,0 +1,7 @@
+package my.com.mandrill.base.reporting.billingAllocationReportsInterEntity;
+
+import my.com.mandrill.base.reporting.reportProcessor.CsvReportProcessor;
+
+public class InterEntityIssuerBalanceInquiryTransactionFees extends CsvReportProcessor {
+
+}
