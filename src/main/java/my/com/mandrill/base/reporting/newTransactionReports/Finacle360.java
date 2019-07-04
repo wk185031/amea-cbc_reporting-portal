de97ab8ca1ec428dcdd2d6161ee84574a3d6397f
karmun <karmun.chai@mandrill.com.my>
2019-07-04 11:30:26 +0800

    Finacle 360

---
+++ src/main/java/my/com/mandrill/base/reporting/newTransactionReports/Finacle360.java
@@ -0,0 +1,7 @@
+package my.com.mandrill.base.reporting.newTransactionReports;
+
+import my.com.mandrill.base.reporting.reportProcessor.CsvReportProcessor;
+
+public class Finacle360 extends CsvReportProcessor {
+
+}
