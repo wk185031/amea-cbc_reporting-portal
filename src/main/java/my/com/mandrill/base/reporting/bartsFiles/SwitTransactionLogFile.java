f9fd3c46a9d9dac8320bbb5fc2abd94d260f89a7
CORP\wk185031 <wk185031@ncr.com>
2021-06-18 17:00:16 +0800

    Fix JIRA 686

--- src/main/java/my/com/mandrill/base/reporting/bartsFiles/SwitTransactionLogFile.java
+++ src/main/java/my/com/mandrill/base/reporting/bartsFiles/SwitTransactionLogFile.java
@@ -21,41 +21,6 @@ public class SwitTransactionLogFile extends TxtReportProcessor {
 	private final Logger logger = LoggerFactory.getLogger(SwitTransactionLogFile.class);
 
 	@Override
-	public void processTxtRecord(ReportGenerationMgr rgm) {
-		logger.debug("In SwitTransactionLogFile.processTxtRecord()");
-		File file = null;
-		String txnDate = null;
-		String fileLocation = rgm.getFileLocation();
-
-		try {
-			if (rgm.isGenerate() == true) {
-				txnDate = rgm.getFileDate().format(DateTimeFormatter.ofPattern(ReportConstants.DATE_FORMAT_03));
-			} else {
-				txnDate = rgm.getYesterdayDate().format(DateTimeFormatter.ofPattern(ReportConstants.DATE_FORMAT_03));
-			}
-
-			if (rgm.errors == 0) {
-				if (fileLocation != null) {
-					File directory = new File(fileLocation);
-					if (!directory.exists()) {
-						directory.mkdirs();
-					}
-					file = new File(
-							rgm.getFileLocation() + rgm.getFileNamePrefix() + txnDate + ReportConstants.TXT_FORMAT);
-					execute(rgm, file);
-				} else {
-					throw new Exception("Path is not configured.");
-				}
-			} else {
-				throw new Exception(
-						"Errors when generating " + rgm.getFileNamePrefix() + txnDate + ReportConstants.TXT_FORMAT);
-			}
-		} catch (Exception e) {
-			logger.error("Errors in generating " + rgm.getFileNamePrefix() + txnDate + ReportConstants.TXT_FORMAT, e);
-		}
-	}
-
-	@Override
 	protected void execute(ReportGenerationMgr rgm, File file) {
 		try {
 			rgm.fileOutputStream = new FileOutputStream(file);
