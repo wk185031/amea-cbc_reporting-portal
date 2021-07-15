76ebaccf1704009bbeb590a2979a50368b594206
ny250043 <ny250043@WMYNY250043-5HI.corp.ncr.com>
2021-07-15 15:47:25 +0800

    Commit initial config before any change for onus report

--- src/main/resources/config/liquibase/report-query/ATM_Transaction_List(Branch)/List_of_ATM_Withdrawals_Report.sql
+++ src/main/resources/config/liquibase/report-query/ATM_Transaction_List(Branch)/List_of_ATM_Withdrawals_Report.sql
@@ -1,17 +1,85 @@
 -- Tracking				Date			Name	Description
 -- CBCAXUPISSLOG-527 	05-JUL-2021		GS		Initial from UAT env
 -- CBCAXUPISSLOG-527 	05-JUL-2021		GS		Modify Trace No pad length to 6 digits
+-- Report revision		15-JUL-2021		NY		Get latest config from UAT env for other column
 
 DECLARE
+    i_HEADER_FIELDS CLOB;
     i_BODY_FIELDS CLOB;
+    i_TRAILER_FIELDS CLOB;
+	i_BODY_QUERY CLOB;
+	i_TRAILER_QUERY CLOB;
 BEGIN 
 
 -- List of ATM Withdrawals Report
+    i_HEADER_FIELDS := TO_CLOB('[{"sequence":1,"sectionName":"1","fieldName":"Bank Code","csvTxtLength":"6","pdfLength":"6","fieldType":"String","defaultValue":"0010","firstField":true,"leftJustified":true,"padFieldLength":0},{"sequence":2,"sectionName":"2","fieldName":"Bank Name","csvTxtLength":"56","pdfLength":"56","fieldType":"String","defaultValue":"CHINA BANK CORPORATION","leftJustified":true,"padFieldLength":0},{"sequence":3,"sectionName":"3","fieldName":"File Name1","csvTxtLength":"54","pdfLength":"54","fieldType":"String","defaultValue":"LIST OF ATM WITHDRAWALS REPORT","leftJustified":true,"padFieldLength":0},{"sequence":4,"sectionName":"4","fieldName":"Todays Date","csvTxtLength":"15","pdfLength":"15","fieldType":"String","defaultValue":"TODAYS DATE","leftJustified":true,"padFieldLength":0},{"sequence":5,"sectionName":"5","fieldName":"Todays Date Value","csvTxtLength":"19","pdfLength":"19","fieldType":"Date","defaultValue":"","fieldFormat":"MM/dd/yyyy","leftJustified":true,"padFieldLength":0},{"sequence":6,"sectionName":"6","fieldName":"PAGE","csvTxtLength":"5","pdfLength":"5","fieldType":"String","defaultValue":"PAGE","leftJustified":true,"padFieldLength":0},{"sequence":7,"sectionName":"7","fieldName":"Page Number","csvTxtLength":"5","pdfLength":"5","fieldType":"String","defaultValue":"","eol":true,"leftJustified":true,"padFieldLength":0},{"sequence":8,"sectionName":"8","fieldName":"BRANCH CODE","csvTxtLength":"6","pdfLength":"6","fieldType":"String","defaultValue":"","firstField":true,"leftJustified":true,"padFieldLength":0},{"sequence":9,"sectionName":"9","fieldName":"BRANCH NAME","csvTxtLength":"111","pdfLength":"111","fieldType":"String","defaultValue":"","leftJustified":true,"padFieldLength":0},{"sequence":10,"sectionName":"10","fieldName":"As Of Date","csvTxtLength":"14","pdfLength":"14","fieldType":"String","defaultValue":"AS OF DATE","leftJustified":true,"padFieldLength":0},{"sequence":11,"sectionName":"11","fieldName":"As of Date Value","csvTxtLength":"17","pdfLength":"17","fieldType":"Date","fieldFormat":"MM/dd/yyyy","leftJustified":true,"padFieldLength":0},{"sequence":12,"sectionName":"12","fieldName":"EFP000-0","csvTxtLength":"8","pdfLength":"8","fieldType":"String","defaultValue":"EFP000-0","eol":true,"leftJustified":true,"padFieldLength":0},{"sequence":13,"sectionName":"13","fieldName":"Space1","csvTxtLength":"120","pdfLength":"120","fieldType":"String","firstField":true,"leftJustified":true,"padFieldLength":0},{"sequence":14,"sectionName":"14","fieldName":"RunDate","csvTxtLength":"11","pdfLength":"11","fieldType":"String","defaultValue":"RUNDATE","leftJustified":true,"padFieldLength":0},{"sequence":15,"sectionName":"15","fieldName":"RunDate Value","csvTxtLength":"11","pdfLength":"11","fieldType":"Date","fieldFormat":"MM/dd/yyyy","leftJustified":true,"padFieldLength":0},{"sequence":16,"sectionName":"16","fieldName":"Time","csvTxtLength":"6","pdfLength":"6","fieldType":"String","defaultValue":"TIME","leftJustified":true,"padFieldLength":0},{"sequence":17,"sectionName":"17","fieldName":"Time Value","eol":true,"fieldType":"Date","fieldFormat":"HH:mm:ss","csvTxtLength":"10","pdfLength":"10","leftJustified":true,"padFieldLength":0}]');
 	i_BODY_FIELDS := TO_CLOB('[{"sequence":1,"sectionName":"1","fieldName":"DATE","csvTxtLength":"11","pdfLength":"11","fieldType":"String","defaultValue":"DATE","bodyHeader":true,"firstField":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":2,"sectionName":"2","fieldName":"TIME","csvTxtLength":"10","pdfLength":"10","fieldType":"String","defaultValue":"TIME","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":3,"sectionName":"3","fieldName":"SEQ","csvTxtLength":"7","pdfLength":"7","fieldType":"String","defaultValue":"SEQ","firstField":false,"bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":4,"sectionName":"4","fieldName":"TRACE","csvTxtLength":"8","pdfLength":"8","fieldType":"String","defaultValue":"TRACE","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":5,"sectionName":"5","fieldName":"TRAN","csvTxtLength":"6","pdfLength":"6","fieldType":"String","defaultValue":"TRAN","bodyHeader":true,"fieldFormat":"","leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":6,"sectionName":"6","fieldName":"ACCT","csvTxtLength":"6","pdfLength":"6","fieldType":"String","defaultValue":"ACCT","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":7,"sectionName":"7","fieldName":"BANK","csvTxtLength":"17","pdfLength":"17","fieldType":"String","defaultValue":"BANK","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":8,"sectionName":"8","fieldName":"CARD","csvTxtLength":"20","pdfLength":"20","fieldType":"String","defaultValue":"CARD","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":9,"sectionName":"9","fieldName":"ACCOUNT","csvTxtLength":"19","pdfLength":"19","fieldType":"String","defaultValue":"ACCOUNT","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":10,"sectionName":"10","fieldName":"TRANS","csvTxtLength":"11","pdfLength":"11","fieldType":"String","defaultValue":"TRANS","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":11,"sectionName":"11","fieldName":"RESPONSE","csvTxtLength":"13","pdfLength":"13","fieldType":"String","defaultValue":"RESPONSE","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":12,"sectionName":"12","fieldName":"REMARKS","csvTxtLength":"21","pdfLength":"21","fieldType":"String","defaultValue":"REMARKS","bodyHeader":true,"eol":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":13,"sectionName":"13","fieldName":"","csvTxtLength":"21","pdfLength":"21","fieldType":"String","firstField":false,"bodyHeader":true,"eol":false,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":14,"sectionName":"14","fieldName":"NO","csvTxtLength":"8","pdfLength":"8","fieldType":"String","defaultValue":"NO","firstField":true,"bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":15,"sectionName":"15","fieldName":"NO","csvTxtLength":"7","pdfLength":"7","fieldType":"String","defaultValue":"NO","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":16,"sectionName":"16","fieldName":"MNEM","csvTxtLength":"6","pdfLength":"6","fieldType":"String","defaultValue":"MNEM","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":17,"sectionName":"17","fieldName":"TYPE","csvTxtLength":"6","pdfLength":"6","fieldType":"String","defaultValue":"TYPE","bodyHeader":true,"eol":false,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":18,"sectionName":"18","fieldName":"MNEM","csvTxtLength":"16","pdfLength":"16","fieldType":"String","defaultValue":"MNEM","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":19,"sectionName":"19","fieldName":"NUMBER","csvTxtLength":"21","pdfLength":"21","fieldType":"String","defaultValue":"NUMBER","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":20,"sectionName":"20","fieldName":"NUMBER","csvTxtLength":"19","pdfLength":"19","fieldType":"String","defaultValue":"NUMBER","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":21,"sectionName":"21","fieldName":"AMOUNT","csvTxtLength":"13","pdfLength":"13","fieldType":"String","bodyHeader":true,"defaultValue":"AMOUNT","leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":22,"sectionName":"22","fieldName":"CODE","csvTxtLength":"10","pdfLength":"10","fieldType":"String","defaultValue":"CODE","bodyHeader":true,"eol":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":23,"sectionName":"23","fieldName":"DATE","csvTxtLength":"12","pdfLength":"12","fieldType":"Date","fieldFormat":"MM/dd/yyyy","leftJustified":false,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":24,"sectionName":"24","fieldName":"TIME","csvTxtLength":"10","pdfLength":"10","fieldType":"Date","fieldFormat":"HH:mm:ss","leftJustified":false,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":25,"sectionName":"25","fieldName":"SEQ NUMBER","csvTxtLength":"8","pdfLength":"8","fieldType":"String","firstField":true,"defaultValue":"","leftJustified":false,"padFieldLength":"6","decrypt":false,"decryptionKey":null,"padFieldType":"Leading","padFieldValue":"Zeros"},{"sequence":26,"sectionName":"26","fieldName":"TRACE NUMBER","csvTxtLength":"8","pdfLength":"8","fieldType":"String","defaultValue":"","leftJustified":false,"padFieldLength":"6","decrypt":false,"decryptionKey":null,"padFieldType":"Leading","padFieldValue":"Zeros"},{"sequence":27,"sectionName":"27","fieldName":"TRAN MNEM","csvTxtLength":"6","pdfLength":"6","fieldType":"String","fieldFormat":"","leftJustified":false,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":28,"sectionName":"28","fieldName":"TYPE","csvTxtLength":"6","pdfLength":"6","fieldType":"String","eol":false,"leftJustified":false,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":29,"sectionName":"29","fieldName":"BANK MNEM","csvTxtLength":"6","pdfLength":"6","fieldType":"String","leftJustified":false,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":30,"sectionName":"30","fieldName":"ATM CARD NUMBER","csvTxtLength":"25","pdfLength":"25","fieldType":"String","leftJustified":false,"padFieldLength":"19","decrypt":true,"decryptionKey":"TRL_PAN_EKY_ID","padFieldType":"Leading","padFieldValue":"Zeros"},{"sequence":31,"sectionName":"31","fieldName":"FROM ACCOUNT NO","csvTxtLength":"20","pdfLength":"20","fieldType":"String","leftJustified":false,"padFieldLength":"16","decrypt":true,"decryptionKey":"TRL_ACCOUNT_1_ACN_ID_EKY_ID","padFieldType":"Leading","padFieldValue":"Zeros"},{"sequence":32,"sectionName":"32","fieldName":"AMOUNT","csvTxtLength":"18","pdfLength":"18","fieldType":"Decimal","fieldFormat":"#,##0.00","leftJustified":false,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":33,"sectionName":"33","fieldName":"VOID CODE","csvTxtLength":"6","pdfLength":"6","fieldType":"String","leftJustified":false,"padFieldLength":"3","decrypt":false,"decryptionKey":null,"padFieldType":"Leading","padFieldValue":"Zeros"},{"sequence":34,"sectionName":"34","fieldName":"COMMENT","csvTxtLength":"30","pdfLength":"30","fieldType":"String","eol":true,"leftJustified":false,"padFieldLength":0,"decrypt":false,"decryptionKey":null}]');
+	i_TRAILER_FIELDS := TO_CLOB('[]');
+	i_BODY_QUERY := TO_CLOB('
+		
+    SELECT
+      TXN.TRL_TQU_ID "TXN QUALIFIER",
+      ABR.ABR_CODE "BRANCH CODE",
+      ABR.ABR_NAME "BRANCH NAME",
+      SUBSTR(AST.AST_TERMINAL_ID, -4) "TERMINAL",
+      AST.AST_ALO_LOCATION_ID "LOCATION",
+      CTR.CTR_CHANNEL "CHANNEL",
+      TXN.TRL_DEST_STAN "SEQ NUMBER",
+      SUBSTR(TXN.TRL_RRN, 9, 4) "TRACE NUMBER",
+      TXN.TRL_PAN "ATM CARD NUMBER",
+      TXN.TRL_PAN_EKY_ID,
+      TXN.TRL_DATETIME_LOCAL_TXN "DATE",
+      TXN.TRL_DATETIME_LOCAL_TXN "TIME",
+      CASE 
+	    WHEN TXN.TRL_ISS_NAME IS NULL AND TXN.TRL_TQU_ID = ''R'' 
+	        THEN (SELECT CTR_REV_MNEM FROM CBC_TRAN_CODE WHERE CTR_CODE=TXN.TRL_TSC_CODE AND CTR_CHANNEL=''BNT'' AND CTR_DEBIT_CREDIT = ''DEBIT'') 
+	    WHEN TXN.TRL_ISS_NAME IS NULL AND TXN.TRL_TQU_ID <> ''R'' 
+	        THEN (SELECT CTR_MNEM FROM CBC_TRAN_CODE WHERE CTR_CODE=TXN.TRL_TSC_CODE AND CTR_CHANNEL=''BNT'' AND CTR_DEBIT_CREDIT = ''DEBIT'')
+	    WHEN TXN.TRL_ISS_NAME IS NOT NULL AND TXN.TRL_TQU_ID = ''R'' 
+	        THEN CTR.CTR_REV_MNEM
+	    ELSE CTR.CTR_MNEM END AS "TRAN MNEM", 	
+      TXN.TRL_ACCOUNT_1_ACN_ID "FROM ACCOUNT NO",
+      TXN.TRL_ACCOUNT_1_ACN_ID_EKY_ID,
+      CASE WHEN TXN.TRL_ACCOUNT_TYPE_1_ATP_ID = 10 THEN ''SA'' WHEN TXN.TRL_ACCOUNT_TYPE_1_ATP_ID = 20 THEN ''CA'' ELSE '''' END AS "TYPE",
+      CBA.CBA_MNEM "BANK MNEM",
+      TXN.TRL_AMT_TXN "AMOUNT",
+      TXN.TRL_ACTION_RESPONSE_CODE "VOID CODE",
+      ARC.ARC_NAME "COMMENT"
+	FROM
+      TRANSACTION_LOG TXN
+      JOIN TRANSACTION_LOG_CUSTOM TXNC ON TXN.TRL_ID=TXNC.TRL_ID
+      JOIN CBC_TRAN_CODE CTR ON TXN.TRL_TSC_CODE = CTR.CTR_CODE AND TXNC.TRL_ORIGIN_CHANNEL = CTR.CTR_CHANNEL
+      JOIN CBC_BIN CBI ON TXNC.TRL_CARD_BIN = CBI.CBI_BIN
+      JOIN CBC_BANK CBA ON CBI.CBI_CBA_ID = CBA.CBA_ID
+      JOIN AUTH_RESULT_CODE ARC ON TXN.TRL_ACTION_RESPONSE_CODE = ARC.ARC_CODE
+      JOIN ATM_STATIONS AST ON TXN.TRL_CARD_ACPT_TERMINAL_IDENT = AST.AST_TERMINAL_ID
+      JOIN ATM_BRANCHES ABR ON AST.AST_ABR_ID = ABR.ABR_ID
+	WHERE
+      TXN.TRL_TSC_CODE IN (1, 128, 142, 143)
+      AND TXN.TRL_TQU_ID = ''F'' 
+	  AND NVL(TXN.TRL_POST_COMPLETION_CODE, '' '') != ''R''
+      AND AST.AST_TERMINAL_TYPE IN (''ATM'', ''BRM'')
+      AND TXN.TRL_FRD_REV_INST_ID IS NULL
+	  AND (TXN.TRL_ISS_NAME = {V_Iss_Name} AND TXN.TRL_DEO_NAME = {V_Deo_Name})
+      AND {Branch_Code}
+      AND {Terminal}
+      AND {Txn_Date}
+	ORDER BY
+      ABR.ABR_CODE ASC,
+      AST.AST_TERMINAL_ID ASC,
+      TXN.TRL_SYSTEM_TIMESTAMP ASC,
+      TXN.TRL_DEST_STAN ASC,
+      TXN.TRL_RRN ASC		
+	');	
+	i_TRAILER_QUERY := null;
 	
-	UPDATE REPORT_DEFINITION set 
-		RED_BODY_FIELDS = i_BODY_FIELDS
-	where RED_NAME = 'List of ATM Withdrawals Report';
+	UPDATE REPORT_DEFINITION SET 
+		RED_HEADER_FIELDS = i_HEADER_FIELDS,
+		RED_BODY_FIELDS = i_BODY_FIELDS,
+		RED_TRAILER_FIELDS = i_TRAILER_FIELDS,
+		RED_BODY_QUERY = i_BODY_QUERY,
+		RED_TRAILER_QUERY = i_TRAILER_QUERY
+	WHERE RED_NAME = 'List of ATM Withdrawals Report';
 	
 END;
 /
