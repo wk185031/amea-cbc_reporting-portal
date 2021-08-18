9d39413384f54930911f081a36e7e292bc449a6d
Yahaya <ny250043@ncr.com>
2021-08-18 11:28:39 +0800

    Updated disable/enable scheme reports

--- src/main/resources/config/liquibase/NY_disable_scheme_report_20210527.sql
+++ src/main/resources/config/liquibase/NY_disable_scheme_report_20210527.sql
@@ -1,8 +1,8 @@
--- script used to temporary disabling few of scheme report
--- to enable back, please use NY_enable_scheme_report_20210527.sql script
 DELETE FROM REPORT_DEFINITION WHERE RED_NAME IN (
 'CUP Share In Fee Income',
 'JCB Share In Fee Income',
 'VISA Share In Fee Income',
 'MasterCard Share In Fee Income'
 );
+
+commit;
