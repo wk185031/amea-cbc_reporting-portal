1f2e5c030933ed628f8123727752eb5dae555a93
Yahaya <ny250043@ncr.com>
2021-08-23 16:53:10 +0800

    767: Add "-" sign to reversed amount in TRAN

--- src/main/java/my/com/mandrill/base/reporting/bartsFiles/TransactionCountReport.java
+++ src/main/java/my/com/mandrill/base/reporting/bartsFiles/TransactionCountReport.java
@@ -3,12 +3,20 @@ package my.com.mandrill.base.reporting.bartsFiles;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
+import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import my.com.mandrill.base.reporting.ReportConstants;
+import my.com.mandrill.base.reporting.ReportGenerationFields;
 import my.com.mandrill.base.reporting.ReportGenerationMgr;
 import my.com.mandrill.base.reporting.reportProcessor.TxtReportProcessor;
 
@@ -74,4 +82,75 @@ public class TransactionCountReport extends TxtReportProcessor {
 			}
 		}
 	}
+	
+	@Override
+	protected void executeBodyQuery(ReportGenerationMgr rgm) {
+		logger.debug("In ATMTransactionListOnUsAcquirer.executeBodyQuery()");
+		ResultSet rs = null;
+		PreparedStatement ps = null;
+		HashMap<String, ReportGenerationFields> fieldsMap = null;
+		HashMap<String, ReportGenerationFields> lineFieldsMap = null;
+		String query = getBodyQuery(rgm);
+		String txnQualifier = null;
+		logger.info("Query for body line export: {}", query);
+
+		if (query != null && !query.isEmpty()) {
+			try {
+				ps = rgm.connection.prepareStatement(query);
+				rs = ps.executeQuery();
+				fieldsMap = rgm.getQueryResultStructure(rs);
+
+				while (rs.next()) {
+					new StringBuffer();
+					lineFieldsMap = rgm.getLineFieldsMap(fieldsMap);
+					for (String key : lineFieldsMap.keySet()) {
+						ReportGenerationFields field = (ReportGenerationFields) lineFieldsMap.get(key);
+						Object result;
+						try {
+							result = rs.getObject(field.getSource());
+						} catch (SQLException e) {
+							rgm.errors++;
+							logger.error("An error was encountered when trying to write a line", e);
+							continue;
+						}
+						if (result != null) {
+							if (result instanceof Date) {
+								field.setValue(Long.toString(((Date) result).getTime()));
+							} else if (result instanceof oracle.sql.TIMESTAMP) {
+								field.setValue(
+										Long.toString(((oracle.sql.TIMESTAMP) result).timestampValue().getTime()));
+							} else if (result instanceof oracle.sql.DATE) {
+								field.setValue(Long.toString(((oracle.sql.DATE) result).timestampValue().getTime()));
+							} else if (key.equalsIgnoreCase(ReportConstants.TXN_QUALIFIER)) {
+								txnQualifier = result.toString();
+								field.setValue(result.toString());
+							} else if (key.equalsIgnoreCase(ReportConstants.REVERSAL_SIGN)) {
+								if(txnQualifier.equals("R")) {
+									field.setValue(result.toString());
+								} else {
+									field.setValue("");
+								}
+							} else {
+								field.setValue(result.toString());
+							}
+						} else {
+							field.setValue("");
+						}
+					}
+					writeBody(rgm, lineFieldsMap);
+				}
+			} catch (Exception e) {
+				rgm.errors++;
+				logger.error("Error trying to execute the body query", e);
+			} finally {
+				try {
+					ps.close();
+					rs.close();
+				} catch (SQLException e) {
+					rgm.errors++;
+					logger.error("Error closing DB resources", e);
+				}
+			}
+		}
+	}	
 }
