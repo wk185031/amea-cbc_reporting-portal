2fd388d6995e4e5bf65c1868d7990459e3e109b5
Yahaya <ny250043@ncr.com>
2021-09-07 15:28:39 +0800

    DCMS: Finacle 360 query update by input from Opus, DCMS to FCBS status mapping

--- src/main/resources/config/liquibase/report-query/Extract_Files/Finacle_360.sql
+++ src/main/resources/config/liquibase/report-query/Extract_Files/Finacle_360.sql
@@ -2,6 +2,7 @@
 -- CBCAXUPISSLOG-673	01-JULY-2021	WY		Fix empty report generated for Finacle 360
 -- CBCAXUPISSLOG-673	06-JULY-2021	WY		Fix issue data generated in one single row
 -- DCMS					22-AUG-2021		NY		Add date range by card created ts
+-- DCMS					07-SEP-2021		NY		Query update by input from Opus, add status mapping to FCBS status
 
 DECLARE
 	i_HEADER_FIELDS CLOB;
@@ -11,73 +12,103 @@ DECLARE
 	i_TRAILER_QUERY CLOB;
 BEGIN 
 
--- Transaction Count Report
+-- Finacle 360
 	i_HEADER_FIELDS := TO_CLOB('[]');
 	i_BODY_FIELDS := TO_CLOB('[{"sequence":1,"sectionName":"14","fieldName":"CIF ID","csvTxtLength":"50","fieldType":"String","delimiter":"|","defaultValue":"CIF ID","firstField":true,"bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":2,"sectionName":"15","fieldName":"BANK ID","csvTxtLength":"50","fieldType":"String","delimiter":"|","defaultValue":"BANK ID","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":3,"sectionName":"16","fieldName":"CARD / IVRS NUMBER","csvTxtLength":"50","fieldType":"String","delimiter":"|","defaultValue":"CARD / IVRS NUMBER","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null,"tagValue":null},{"sequence":4,"sectionName":"17","fieldName":"ACCOUNT NUMBER","csvTxtLength":"50","fieldType":"String","delimiter":"|","defaultValue":"ACCOUNT NUMBER","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":5,"sectionName":"18","fieldName":"OPERATION FLAG","csvTxtLength":"50","fieldType":"String","delimiter":"|","defaultValue":"OPERATION FLAG","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":6,"sectionName":"19","fieldName":"EMBOSSED NAME","csvTxtLength":"50","fieldType":"String","delimiter":"|","defaultValue":"EMBOSSED NAME","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":7,"sectionName":"20","fieldName":"CARD / IVRS TYPE","csvTxtLength":"50","fieldType":"String","delimiter":"|","defaultValue":"CARD / IVRS TYPE","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":8,"sectionName":"21","fieldName":"CARD SUBTYPE","csvTxtLength":"50","fieldType":"String","delimiter":"|","defaultValue":"CARD SUBTYPE","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":9,"sectionName":"22","fieldName":"CARD / IVRS STATUS","csvTxtLength":"50","fieldType":"String","delimiter":"|","defaultValue":"CARD / IVRS STATUS","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":10,"sectionName":"23","fieldName":"NO. OF JOINT ACCOUNT HOLDERS","csvTxtLength":"50","fieldType":"String","delimiter":"|","defaultValue":"NO. OF JOINT ACCOUNT HOLDERS","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":11,"sectionName":"24","fieldName":"CIF ID","csvTxtLength":"50","fieldType":"String","delimiter":"|","defaultValue":"CIF ID","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":12,"sectionName":"25","fieldName":"ACCOUNT HOLDER TYPE","csvTxtLength":"50","fieldType":"String","delimiter":"|","defaultValue":"ACCOUNT HOLDER TYPE","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":13,"sectionName":"26","fieldName":"DEL / ADD / MOD FLAG","csvTxtLength":"50","fieldType":"String","delimiter":"|","defaultValue":"DEL / ADD / MOD FLAG","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false,"decryptionKey":null,"eol":true},{"sequence":14,"sectionName":"1","fieldName":"CIF","csvTxtLength":"50","fieldType":"String","delimiter":"|","leftJustified":false,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":15,"sectionName":"2","fieldName":"BANK ID","csvTxtLength":"8","fieldType":"String","delimiter":"|","leftJustified":false,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":16,"sectionName":"3","fieldName":"ATM CARD NUMBER","csvTxtLength":"19","fieldType":"String","delimiter":"|","leftJustified":false,"padFieldLength":0,"decrypt":true,"decryptionKey":"DCMS_ENCRYPTION_KEY","tagValue":null},{"sequence":17,"sectionName":"4","fieldName":"FROM ACCOUNT NO","csvTxtLength":"16","fieldType":"String","delimiter":"|","leftJustified":false,"padFieldLength":0,"decrypt":true,"decryptionKey":"DCMS_ENCRYPTION_KEY","tagValue":null},{"sequence":18,"sectionName":"5","fieldName":"OPERATION FLAG","csvTxtLength":"1","fieldType":"String","delimiter":"|","leftJustified":false,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":19,"sectionName":"6","fieldName":"ACCOUNT NAME","csvTxtLength":"80","fieldType":"String","delimiter":"|","leftJustified":false,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":20,"sectionName":"7","fieldName":"CARD TYPE","csvTxtLength":"5","fieldType":"String","delimiter":"|","leftJustified":false,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":21,"sectionName":"8","fieldName":"CARD SUBTYPE","csvTxtLength":"5","fieldType":"String","delimiter":"|","leftJustified":false,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":22,"sectionName":"9","fieldName":"CARD STATUS","csvTxtLength":"1","fieldType":"String","delimiter":"|","leftJustified":false,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":23,"sectionName":"10","fieldName":"NO OF JOINT ACC HOLDERS","csvTxtLength":"2","fieldType":"Integer","delimiter":"|","leftJustified":false,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":24,"sectionName":"11","fieldName":"CIF ID","csvTxtLength":"50","fieldType":"String","delimiter":"|","leftJustified":false,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":25,"sectionName":"12","fieldName":"ACCOUNT HOLDER TYPE","csvTxtLength":"50","fieldType":"String","delimiter":"|","leftJustified":false,"padFieldLength":0,"decrypt":false,"decryptionKey":null},{"sequence":26,"sectionName":"13","fieldName":"FLAG","csvTxtLength":"1","fieldType":"String","delimiter":"|","leftJustified":false,"padFieldLength":0,"decrypt":false,"decryptionKey":null,"eol":true}]');
 	i_TRAILER_FIELDS := TO_CLOB('[]');
 	
-	i_BODY_QUERY := TO_CLOB('				
-select * from
-(select
-	clt.clt_cif_number as "CIF",
-	''{Iss_Name}'' as "BANK ID",
-	crd.crd_number_enc as "ATM CARD NUMBER",
-	crd.CRD_KEY_ROTATION_NUMBER "ROTATION_NUMBER",
-	INS.INS_CODE AS INSTITUTION_ID,
-	bac.bac_account_number as "FROM ACCOUNT NO",
-	''A'' as "OPERATION FLAG",
-	dcr.dcr_embossing_name as "ACCOUNT NAME",
-	''ATM'' as "CARD TYPE",
-	prs.prs_name as "CARD SUBTYPE",
-	sts.sts_name as "CARD STATUS",
+	i_BODY_QUERY := TO_CLOB('
+SELECT 
+	"CIF",
+	"BANK ID",
+	"ATM CARD NUMBER",
+	"ROTATION_NUMBER",
+	"INSTITUTION_ID",
+	"FROM ACCOUNT NO",
+	"OPERATION FLAG",
+	"ACCOUNT NAME",
+	"CARD TYPE",
+	"CARD SUBTYPE",
+	CASE WHEN "CARD STATUS" = ''Active'' THEN ''A''
+	     WHEN "CARD STATUS" = ''Inactive'' THEN ''I''
+		 WHEN "CARD STATUS" = ''Blocked'' THEN ''B''
+		 WHEN "CARD STATUS" = ''Closed'' THEN ''C''
+		 WHEN "CARD STATUS" = ''Stolen'' THEN ''S''
+		 WHEN "CARD STATUS" = ''Damaged'' THEN ''D''
+		 WHEN "CARD STATUS" = ''Replaced'' THEN ''R''
+		 WHEN "CARD STATUS" = ''Lost'' THEN ''L''
+		 WHEN "CARD STATUS" = ''Unusual Activity'' THEN ''U''
+		 WHEN "CARD STATUS" = ''Captured lost'' THEN ''X''
+		 WHEN "CARD STATUS" = ''Blocked-Exceeded PIN Tries'' THEN ''P''
+		 WHEN "CARD STATUS" = ''Suspicious'' THEN ''Z''
+		 ELSE ''''
+		END AS "CARD STATUS",
+	"NO OF JOINT ACC HOLDERS",
+    "JOINT ACC CIF",
+	"JOINT ACCOUNT HOLDER TYPE",
+	"JOINT ACC OPERATION FLAG"
+FROM
+(SELECT
+	CLT.CLT_CIF_NUMBER AS "CIF",
+	''{Iss_Name}'' AS "BANK ID",
+	CRD.CRD_NUMBER_ENC AS "ATM CARD NUMBER",
+	CRD.CRD_KEY_ROTATION_NUMBER "ROTATION_NUMBER",
+	INS.INS_CODE AS "INSTITUTION_ID",
+	BAC.BAC_ACCOUNT_NUMBER AS "FROM ACCOUNT NO",
+	CASE WHEN DCR.DCR_REQUEST_TYPE = ''Manual'' THEN ''A'' ELSE ''M'' END AS "OPERATION FLAG",
+	DCR.DCR_EMBOSSING_NAME AS "ACCOUNT NAME",
+	''ATM'' AS "CARD TYPE",
+	PRS.PRS_CODE AS "CARD SUBTYPE",
+    STS.STS_NAME AS "CARD STATUS",
 	'''' AS "NO OF JOINT ACC HOLDERS",
 	'''' AS "JOINT ACC CIF",
 	'''' AS "JOINT ACCOUNT HOLDER TYPE",
 	'''' AS "JOINT ACC OPERATION FLAG"
-from
-	{DCMS_Schema}.issuance_debit_card_request@{DB_LINK_DCMS} dcr
-	inner join {DCMS_Schema}.issuance_card@{DB_LINK_DCMS} crd on crd.crd_id = dcr.dcr_crd_id
-	inner join {DCMS_Schema}.issuance_client@{DB_LINK_DCMS} clt on clt.clt_id = dcr.dcr_clt_id
-	inner join {DCMS_Schema}.issuance_bank_account@{DB_LINK_DCMS} bac on bac.bac_clt_id = clt.clt_id
-	inner join {DCMS_Schema}.card_program_setup@{DB_LINK_DCMS} prs on prs.prs_id = dcr.dcr_prs_id
-	inner join {DCMS_Schema}.master_status@{DB_LINK_DCMS} sts on sts_id = dcr.dcr_sts_id
-	inner join {DCMS_Schema}.MASTER_INSTITUTIONS@{DB_LINK_DCMS} INS ON crd.CRD_INS_ID = INS.INS_ID
-where 
-	dcr.dcr_ins_id = {Iss_Id} and
-	trunc(crd.crd_created_ts) between To_Date({From_Date},''dd-MM-YY hh24:mi:ss'') And To_Date({To_Date},''dd-MM-YY hh24:mi:ss'')
-union all
-select
-	clt.clt_cif_number as "CIF",
-	''{Iss_Name}'' as "BANK ID",
-	csh.csh_card_number_enc as "ATM CARD NUMBER",
-	csh.CSH_KEY_ROTATION_NUMBER "ROTATION_NUMBER",
+FROM
+    {DCMS_Schema}.ISSUANCE_DEBIT_CARD_REQUEST@{DB_LINK_DCMS} DCR 
+    INNER JOIN {DCMS_Schema}.ISSUANCE_CARD@{DB_LINK_DCMS} CRD ON CRD.CRD_ID = DCR.DCR_CRD_ID
+	INNER JOIN {DCMS_Schema}.ISSUANCE_CLIENT@{DB_LINK_DCMS} CLT ON CLT.CLT_ID = DCR.DCR_CLT_ID
+	INNER JOIN {DCMS_Schema}.ISSUANCE_BANK_ACCOUNT@{DB_LINK_DCMS} BAC ON BAC.BAC_CLT_ID = CLT.CLT_ID
+	INNER JOIN {DCMS_Schema}.CARD_PROGRAM_SETUP@{DB_LINK_DCMS} PRS ON PRS.PRS_ID = DCR.DCR_PRS_ID
+	INNER JOIN {DCMS_Schema}.MASTER_STATUS@{DB_LINK_DCMS} STS ON STS_ID = CRD.CRD_STS_ID
+	INNER JOIN {DCMS_Schema}.MASTER_INSTITUTIONS@{DB_LINK_DCMS} INS ON CRD.CRD_INS_ID = INS.INS_ID
+WHERE 
+	DCR.DCR_INS_ID = {Iss_Id} AND
+	TRUNC(DCR.DCR_UPDATED_TS) BETWEEN TO_DATE({From_Date},''dd-MM-YY hh24:mi:ss'') AND TO_DATE({To_Date},''dd-MM-YY hh24:mi:ss'')
+UNION ALL
+SELECT
+	CLT.CLT_CIF_NUMBER AS "CIF",
+	''{Iss_Name}'' AS "BANK ID",
+	CSH.CSH_CARD_NUMBER_ENC AS "ATM CARD NUMBER",
+	CSH.CSH_KEY_ROTATION_NUMBER "ROTATION_NUMBER",
 	INS.INS_CODE AS INSTITUTION_ID,
-	bac.bac_account_number as "FROM ACCOUNT NO",
-	''A'' as "OPERATION FLAG",
-	ccr.ccr_embossing_name as "ACCOUNT NAME",
-	''CASH'' as "CARD TYPE",
-	prs.prs_name as "CARD SUBTYPE",
-	sts.sts_name as "CARD STATUS",
+	BAC.BAC_ACCOUNT_NUMBER AS "FROM ACCOUNT NO",
+	CASE WHEN CCR.CCR_REQUEST_TYPE = ''Manual'' THEN ''A'' ELSE ''M'' END AS "OPERATION FLAG",
+	CCR.CCR_EMBOSSING_NAME AS "ACCOUNT NAME",
+	''CASH'' AS "CARD TYPE",
+	PRS.PRS_CODE AS "CARD SUBTYPE",
+	STS.STS_NAME AS "CARD STATUS",
 	'''' AS "NO OF JOINT ACC HOLDERS",
 	'''' AS "JOINT ACC CIF",
 	'''' AS "JOINT ACCOUNT HOLDER TYPE",
 	'''' AS "JOINT ACC OPERATION FLAG"
-from
-	{DCMS_Schema}.issuance_cash_card_request@{DB_LINK_DCMS} ccr
-	inner join {DCMS_Schema}.issuance_cash_card@{DB_LINK_DCMS} csh on csh.csh_id = ccr.ccr_csh_id
-	inner join {DCMS_Schema}.issuance_client@{DB_LINK_DCMS} clt on clt.clt_id = ccr.ccr_clt_id
-	inner join {DCMS_Schema}.issuance_bank_account@{DB_LINK_DCMS} bac on bac.bac_clt_id = clt.clt_id
-	inner join {DCMS_Schema}.card_program_setup@{DB_LINK_DCMS} prs on prs.prs_id = ccr.ccr_prs_id
-	inner join {DCMS_Schema}.master_status@{DB_LINK_DCMS} sts on sts_id = ccr.ccr_sts_id
-	inner join {DCMS_Schema}.MASTER_INSTITUTIONS@{DB_LINK_DCMS} INS ON csh.CSH_INS_ID = INS.INS_ID
-where 
-	ccr.ccr_ins_id = {Iss_Id} and
-	trunc(csh.csh_created_ts) between To_Date({From_Date},''dd-MM-YY hh24:mi:ss'') And To_Date({To_Date},''dd-MM-YY hh24:mi:ss'')
-) tb
-	where tb."ROTATION_NUMBER" is not null
-order by
-	tb.cif, tb."ATM CARD NUMBER"		
+FROM
+    {DCMS_Schema}.ISSUANCE_CASH_CARD_REQUEST@{DB_LINK_DCMS} CCR
+	INNER JOIN {DCMS_Schema}.ISSUANCE_CASH_CARD@{DB_LINK_DCMS} CSH ON CSH.CSH_ID = CCR.CCR_CSH_ID
+	INNER JOIN {DCMS_Schema}.ISSUANCE_CLIENT@{DB_LINK_DCMS} CLT ON CLT.CLT_ID = CCR.CCR_CLT_ID
+	INNER JOIN {DCMS_Schema}.ISSUANCE_BANK_ACCOUNT@{DB_LINK_DCMS} BAC ON BAC.BAC_CLT_ID = CLT.CLT_ID
+	INNER JOIN {DCMS_Schema}.CARD_PROGRAM_SETUP@{DB_LINK_DCMS} PRS ON PRS.PRS_ID = CCR.CCR_PRS_ID
+	INNER JOIN {DCMS_Schema}.MASTER_STATUS@{DB_LINK_DCMS} STS ON STS_ID = CSH.CSH_STS_ID
+	INNER JOIN {DCMS_Schema}.MASTER_INSTITUTIONS@{DB_LINK_DCMS} INS ON CSH.CSH_INS_ID = INS.INS_ID
+WHERE 
+	CCR.CCR_INS_ID = {Iss_Id} AND
+	TRUNC(CCR.CCR_UPDATED_TS) BETWEEN TO_DATE({From_Date},''dd-MM-YY hh24:mi:ss'') AND TO_DATE({To_Date},''dd-MM-YY hh24:mi:ss'')
+) TB
+	WHERE TB."ROTATION_NUMBER" IS NOT NULL
+ORDER BY
+	TB.CIF, TB."ATM CARD NUMBER"		
 	');	
+
 	i_TRAILER_QUERY := null;
 	
 	UPDATE REPORT_DEFINITION SET 
