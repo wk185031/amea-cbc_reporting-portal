2e943c61cd9399498f07c1c2ca3a842c37098929
CORP\wk185031 <wk185031@ncr.com>
2021-09-14 14:23:57 +0800

    Fix JIRA 917

--- src/main/resources/config/liquibase/report-query/ECM_Reports/ATM_Daily_Exception_Message.sql
+++ src/main/resources/config/liquibase/report-query/ECM_Reports/ATM_Daily_Exception_Message.sql
@@ -1,6 +1,7 @@
 -- Tracking				Date			Name	Description
 -- Revise Report	 	25-JULY-2021	WY		Revise report according to requirement
 -- JIRA 841				10-AUG-2021		WY		Maintain uniform date
+-- JIRA 917				13-SEPT-2021	WY		Extract Exception message based on message format
 
 DECLARE
 	
@@ -12,23 +13,40 @@ BEGIN
 	i_BODY_FIELDS := TO_CLOB('[{"sequence":1,"sectionName":"1","fieldName":"Region","csvTxtLength":"10","pdfLength":"10","fieldType":"String","delimiter":";","fieldFormat":"","defaultValue":"Region","firstField":true,"bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false},{"sequence":2,"sectionName":"2","fieldName":"Terminal ID","csvTxtLength":"10","pdfLength":"10","fieldType":"String","delimiter":";","fieldFormat":"","defaultValue":"Terminal ID","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false},{"sequence":3,"sectionName":"3","fieldName":"Terminal Name","csvTxtLength":"10","pdfLength":"10","fieldType":"String","delimiter":";","fieldFormat":"","defaultValue":"Terminal Name","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false},{"sequence":4,"sectionName":"4","fieldName":"Date/Time","csvTxtLength":"10","pdfLength":"10","fieldType":"String","delimiter":";","fieldFormat":"","defaultValue":"Date/Time","bodyHeader":true,"leftJustified":true,"padFieldLength":0,"decrypt":false},{"sequence":5,"sectionName":"5","fieldName":"Exception Message","csvTxtLength":"10","pdfLength":"10","fieldType":"String","delimiter":";","fieldFormat":"","defaultValue":"Exception Message","bodyHeader":true,"eol":true,"leftJustified":true,"padFieldLength":0,"decrypt":false},{"sequence":6,"sectionName":"6","fieldName":"REGION","csvTxtLength":"10","pdfLength":"10","fieldType":"String","delimiter":";","fieldFormat":"","firstField":true,"leftJustified":false,"padFieldLength":0,"decrypt":false},{"sequence":7,"sectionName":"7","fieldName":"TERMINAL","csvTxtLength":"10","pdfLength":"10","fieldType":"String","delimiter":";","fieldFormat":"","leftJustified":false,"padFieldLength":0,"decrypt":false},{"sequence":8,"sectionName":"8","fieldName":"LOCATION","csvTxtLength":"10","pdfLength":"10","fieldType":"String","delimiter":";","fieldFormat":"","leftJustified":false,"padFieldLength":0,"decrypt":false},{"sequence":9,"sectionName":"9","fieldName":"DATE TIME","csvTxtLength":"10","pdfLength":"10","fieldType":"Date Time","delimiter":";","fieldFormat":"MM/dd/yyyy HH:mm","leftJustified":false,"padFieldLength":0,"decrypt":false},{"sequence":10,"sectionName":"10","fieldName":"EXCEPTION","csvTxtLength":"10","pdfLength":"10","fieldType":"String","delimiter":";","fieldFormat":"","eol":true,"leftJustified":false,"padFieldLength":0,"decrypt":false}]');
 	
 	i_BODY_QUERY := TO_CLOB('
-SELECT
+	SELECT * FROM(
+  SELECT
       AST.AST_ARE_NAME "REGION",
       AST.AST_TERMINAL_ID "TERMINAL",
       AST.AST_ALO_LOCATION_ID "LOCATION",
       AJL.AJL_TIMESTAMP "DATE TIME",
-      AJL.AJL_EJ_DATA "EXCEPTION"
+      REGEXP_SUBSTR(AJL.AJL_EJ_DATA,''(\d){4}+\*[U|S]+\*[A-Z|a-z]+\*(\d){1,13},M-(\d){0,2},R-(\d){0,5}'')  "EXCEPTION"
 	FROM
       ATM_STATIONS AST
       JOIN ATM_JOURNAL_LOG AJL ON AST.AST_ID = AJL.AJL_AST_ID
 	  JOIN DEVICE_ESTATE_OWNER DEO ON AST.AST_DEO_ID = DEO.DEO_ID
-      AND {Txn_Date}
-	WHERE (AJL.AJL_EJ_DATA LIKE ''%*U*%'' OR AJL.AJL_EJ_DATA LIKE ''%*S*%'' OR AJL.AJL_EJ_DATA LIKE ''%*2*%'')
-	AND DEO.DEO_NAME = {V_Deo_Name}
-	ORDER BY
-      AST.AST_ARE_NAME ASC,
-      AST.AST_TERMINAL_ID ASC,
-      AJL.AJL_TIMESTAMP ASC');
+    WHERE (AJL.AJL_EJ_DATA LIKE ''%*U*%'' OR AJL.AJL_EJ_DATA LIKE ''%*S*%'')
+    AND DEO.DEO_NAME = {V_Deo_Name}
+	AND {Txn_Date}
+    UNION
+    SELECT
+      AST.AST_ARE_NAME "REGION",
+      AST.AST_TERMINAL_ID "TERMINAL",
+      AST.AST_ALO_LOCATION_ID "LOCATION",
+      AJL.AJL_TIMESTAMP "DATE TIME",
+      REGEXP_SUBSTR(AJL.AJL_EJ_DATA,''\*(\d){4}\*2\*[A-Z|1-9]+\*N'')  "EXCEPTION"
+      FROM
+      ATM_STATIONS AST
+      JOIN ATM_JOURNAL_LOG AJL ON AST.AST_ID = AJL.AJL_AST_ID
+	  JOIN DEVICE_ESTATE_OWNER DEO ON AST.AST_DEO_ID = DEO.DEO_ID
+       WHERE AJL.AJL_EJ_DATA LIKE ''%*2*%''
+       AND DEO.DEO_NAME = {V_Deo_Name}
+	   AND {Txn_Date}
+       )
+       WHERE "EXCEPTION" IS NOT NULL
+	   ORDER BY
+      "REGION" ASC,
+      "TERMINAL" ASC,
+      "DATE TIME" ASC');
 	
 	UPDATE REPORT_DEFINITION set 
 		RED_BODY_FIELDS = i_BODY_FIELDS,
