11c4a71b11cec8b726df051a68198d1b68656b48
Lau JiunLock <lau.jiunlock@ncr.com>
2021-10-07 10:43:30 +0000

    Revert "no message"
    This reverts commit d5772c40454699a7dec2ad877da47ef69a66f741

--- src/main/resources/config/liquibase/REPORT_DEFINITION_20211007.sql
+++ src/main/resources/config/liquibase/REPORT_DEFINITION_20211007.sql
@@ -1,1 +1,1 @@
-update REPORT_DEFINITION SET RED_FILE_NAME_PREFIX = 'CBC_' WHERE RED_NAME='Bills Payment Extract Files (Summary)' AND RED_INS_ID=22;
+update REPORT_DEFINITION SET RED_FILE_NAME_PREFIX = 'CBC_' WHERE RED_NAME='Bills Payment Extract Files (Summary)' AND RED_INS_ID='22';
