19233ff74e25c8424d152431b79e42cf8c83d904
CORP\wk185031 <wk185031@ncr.com>
2021-11-23 05:06:25 +0800

    Added table schema

--- src/main/resources/config/liquibase/auth-scripts/979_CBC_BIN_Delete_International_Bins.sql
+++ src/main/resources/config/liquibase/auth-scripts/979_CBC_BIN_Delete_International_Bins.sql
@@ -1,17 +1,17 @@
-select count(*) from cbc_bin;
+select count(*) from ATM5ADM.cbc_bin;
 
-create table cbc_bin_bkup as (select * from cbc_bin);
+create table ATM5ADM.cbc_bin_bkup as (select * from ATM5ADM.cbc_bin);
 
-truncate table cbc_bin cascade;
+truncate table ATM5ADM.cbc_bin cascade;
 
-insert into cbc_bin (select * from cbc_bin_bkup where cbi_cba_id not in (select cba_id from cbc_bank where cba_code in ('0728','0850')));
+insert into ATM5ADM.cbc_bin (select * from ATM5ADM.cbc_bin_bkup where cbi_cba_id not in (select cba_id from ATM5ADM.cbc_bank where cba_code in ('0728','0850')));
   
-insert into cbc_bin (select * from cbc_bin_bkup where cbi_cba_id in (select cba_id from cbc_bank where cba_code in ('0728','0850'))
+insert into ATM5ADM.cbc_bin (select * from ATM5ADM.cbc_bin_bkup where cbi_cba_id in (select cba_id from ATM5ADM.cbc_bank where cba_code in ('0728','0850'))
 AND CBI_LAST_UPDATE_TS > TO_DATE('20211015 23:59:59','YYYYMMDD HH24:MI:SS'));
   
-insert into cbc_bin (select * from cbc_bin_bkup where cbi_cba_id in (select cba_id from cbc_bank where cba_code in ('0728','0850'))
+insert into ATM5ADM.cbc_bin (select * from ATM5ADM.cbc_bin_bkup where cbi_cba_id in (select cba_id from ATM5ADM.cbc_bank where cba_code in ('0728','0850'))
 AND CBI_LAST_UPDATE_TS < TO_DATE('20211015 00:00:00','YYYYMMDD HH24:MI:SS'));
 
 commit;
 
-select count(*) from cbc_bin;
+select count(*) from ATM5ADM.cbc_bin;
