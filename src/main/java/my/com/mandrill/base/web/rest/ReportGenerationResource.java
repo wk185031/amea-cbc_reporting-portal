b28ec9150a0e7834547feb8957ebd03d6e3197d9
Kok Weng <kokweng.chong@mandrill.com.my>
2022-04-18 16:35:09 +0800

    CBCAXUPISSLOG-1286: Prevent branch user from generate report

--- src/main/java/my/com/mandrill/base/web/rest/ReportGenerationResource.java
+++ src/main/java/my/com/mandrill/base/web/rest/ReportGenerationResource.java
@@ -114,6 +114,11 @@ public class ReportGenerationResource {
 				SecurityUtils.getCurrentUserLogin().orElse(""), institutionId, reportCategoryId, reportId,
 				startDateTime, endDateTime);
 
+		String branchCode = getUserBranchCode();
+		if (branchCode != null && !branchCode.trim().isEmpty()) {
+			throw new IllegalArgumentException("You do not have permission to generate report.");
+		}
+		
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
 		LocalDateTime inputStartDateTime = LocalDateTime.parse(startDateTime, formatter);
