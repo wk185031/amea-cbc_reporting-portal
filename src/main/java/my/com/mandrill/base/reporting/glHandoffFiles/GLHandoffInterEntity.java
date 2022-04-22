c3bf5e8c67103dc0a6cce7aa0dba9a8437f9eca6
CORP\wk185031 <wk185031@ncr.com>
2022-04-22 16:57:55 +0800

    Fix for JIRA 1316 - Remove records where transmitting and receiving are from CBC bank but perform txns in CBS terminal and vice versa

--- src/main/java/my/com/mandrill/base/reporting/glHandoffFiles/GLHandoffInterEntity.java
+++ src/main/java/my/com/mandrill/base/reporting/glHandoffFiles/GLHandoffInterEntity.java
@@ -142,28 +142,39 @@ public class GLHandoffInterEntity extends BatchProcessor {
 		case ReportConstants.INTER_ENTITY_FUND_TRANSFER_DR:
 			ReportGenerationFields channelDR = new ReportGenerationFields(ReportConstants.PARAM_CHANNEL,
 					ReportGenerationFields.TYPE_STRING,
-					"TXN.TRL_TSC_CODE IN (40,42,48) AND TXN.TRL_ISS_NAME = '" + rgm.getInstitution()
-							+ "' AND (((TXN.TRL_DEO_NAME = '" + rgm.getInstitution()
-							+ "' OR LPAD(TXN.TRL_ACQR_INST_ID, 10, '0') = '" + ins_id + "')"
-							+ " AND LPAD(TXN.TRL_FRD_REV_INST_ID, 10, '0') = '" + ie_ins_id + "') OR "
-							+ "((TXN.TRL_DEO_NAME = '" + ie_ins_name + "' OR LPAD(TXN.TRL_ACQR_INST_ID, 10, '0') = '"
-							+ ie_ins_id + "')" + " AND (LPAD(TXN.TRL_FRD_REV_INST_ID, 10, '0') IN ('" + ie_ins_id
-							+ "','" + ins_id + "')"
-							+ "OR (TXN.TRL_FRD_REV_INST_ID IS NULL AND TXN.TRL_TSC_CODE = 40)))) ");
+//					"TXN.TRL_TSC_CODE IN (40,42,48) AND TXN.TRL_ISS_NAME = '" + rgm.getInstitution()
+//							+ "' AND (((TXN.TRL_DEO_NAME = '" + rgm.getInstitution()
+//							+ "' OR LPAD(TXN.TRL_ACQR_INST_ID, 10, '0') = '" + ins_id + "')"
+//							+ " AND LPAD(TXN.TRL_FRD_REV_INST_ID, 10, '0') = '" + ie_ins_id + "') OR "
+//							+ "((TXN.TRL_DEO_NAME = '" + ie_ins_name + "' OR LPAD(TXN.TRL_ACQR_INST_ID, 10, '0') = '"
+//							+ ie_ins_id + "')" + " AND (LPAD(TXN.TRL_FRD_REV_INST_ID, 10, '0') IN ('" + ie_ins_id + "')"
+////							+ "','" + ins_id + "')"
+//							+ "OR (TXN.TRL_FRD_REV_INST_ID IS NULL AND TXN.TRL_TSC_CODE = 40)))) ");
+					"TXN.TRL_TSC_CODE IN (40,42,48) AND TXN.TRL_ISS_NAME = '" + rgm.getInstitution() + "'"
+							+ "AND (TXN.TRL_DEO_NAME IN ( '" + rgm.getInstitution() + "','" + ie_ins_name + "')"
+							+ " OR LPAD(TXN.TRL_ACQR_INST_ID, 10, '0') IN ('" + ins_id + "','" + ie_ins_id + "'))"
+							+ " AND (LPAD(TXN.TRL_FRD_REV_INST_ID, 10, '0')) IN ('" + ie_ins_id + "')");
+
 			getGlobalFileFieldsMap().put(channelDR.getFieldName(), channelDR);
 			break;
 		case ReportConstants.INTER_ENTITY_FUND_TRANSFER_CR:
 			ReportGenerationFields channelCR = new ReportGenerationFields(ReportConstants.PARAM_CHANNEL,
 					ReportGenerationFields.TYPE_STRING,
-					"TXN.TRL_TSC_CODE IN (40, 42, 44, 48) " + " AND ((TXN.TRL_ISS_NAME = '" + rgm.getInstitution() + "'"
-							+ " AND (TXN.TRL_DEO_NAME = '" + ie_ins_name
-							+ "' OR LPAD(TXN.TRL_ACQR_INST_ID, 10, '0') = '" + ie_ins_id + "') "
-							+ " AND (LPAD(TXN.TRL_FRD_REV_INST_ID, 10, '0') = '" + ins_id
-							+ "' OR (TXN.TRL_FRD_REV_INST_ID IS NULL AND TXN.TRL_TSC_CODE = 40)))"
-							+ " OR (TXN.TRL_ISS_NAME = '" + ie_ins_name + "'" + " AND (TXN.TRL_DEO_NAME IN ('"
-							+ rgm.getInstitution() + "' , '" + ie_ins_name
-							+ "') OR LPAD(TXN.TRL_ACQR_INST_ID, 10, '0') IN ('" + ie_ins_id + "','" + ins_id + "'))"
-							+ " AND LPAD(TXN.TRL_FRD_REV_INST_ID, 10, '0') = '" + ins_id + "'))");
+//					"TXN.TRL_TSC_CODE IN (40, 42, 44, 48) "
+//					+ " AND ((TXN.TRL_ISS_NAME = '" + rgm.getInstitution() + "'"
+//					+ " AND (TXN.TRL_DEO_NAME = '" + ie_ins_name + "' OR LPAD(TXN.TRL_ACQR_INST_ID, 10, '0') = '" + ie_ins_id + "') "
+//					+ " AND (LPAD(TXN.TRL_FRD_REV_INST_ID, 10, '0') = '" + ins_id + "' OR (TXN.TRL_FRD_REV_INST_ID IS NULL AND TXN.TRL_TSC_CODE = 40)))"
+//					+ " OR ("
+//							+ " AND TXN.TRL_ISS_NAME = '" + ie_ins_name + "'" + " AND (TXN.TRL_DEO_NAME IN ('"
+//							+ rgm.getInstitution() + "' , '" + ie_ins_name
+//							+ "') OR LPAD(TXN.TRL_ACQR_INST_ID, 10, '0') IN ('" + ie_ins_id + "','" + ins_id + "'))"
+//							+ " AND LPAD(TXN.TRL_FRD_REV_INST_ID, 10, '0') = '" + ins_id + "'");
+//							+ ")");
+					"TXN.TRL_TSC_CODE IN (40,42,48) AND TXN.TRL_ISS_NAME = '" + ie_ins_name + "'"
+							+ "AND (TXN.TRL_DEO_NAME IN ( '" + rgm.getInstitution() + "','" + ie_ins_name + "')"
+							+ " OR LPAD(TXN.TRL_ACQR_INST_ID, 10, '0') IN ('" + ins_id + "','" + ie_ins_id + "'))"
+							+ " AND (LPAD(TXN.TRL_FRD_REV_INST_ID, 10, '0')) IN ('" + ins_id + "')");
+
 			getGlobalFileFieldsMap().put(channelCR.getFieldName(), channelCR);
 			rgm.setBodyQuery(rgm.getBodyQuery().replace("TXN.TRL_ACCOUNT_1_ACN_ID", "TXN.TRL_ACCOUNT_2_ACN_ID"));
 			break;
