fc2af29d28406a8430807ef8a5e1c344289ba4f8
CORP\lj185057 <lj185057@ncr.com>
2022-07-29 16:28:45 +0800

    FIX JIRA-1167
    Fix branch code filter

--- src/main/java/my/com/mandrill/base/reporting/cashCardReports/CashCardApprovedTransactions.java
+++ src/main/java/my/com/mandrill/base/reporting/cashCardReports/CashCardApprovedTransactions.java
@@ -774,7 +774,7 @@ public class CashCardApprovedTransactions extends PdfReportProcessor {
 			// rgm.setBodyQuery(rgm.getTmpBodyQuery().replace("AND {" +
 			// ReportConstants.PARAM_TERMINAL + "}", ""));
 			ReportGenerationFields branchCode = new ReportGenerationFields(ReportConstants.PARAM_BRANCH_CODE,
-					ReportGenerationFields.TYPE_STRING, "BRC.BRC_CODE = '" + filterByBranchCode + "'");
+					ReportGenerationFields.TYPE_STRING, "TXNC.TRL_CARD_BRANCH = '" + filterByBranchCode + "'");
 			getGlobalFileFieldsMap().put(branchCode.getFieldName(), branchCode);
 		}
 
